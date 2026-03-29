493259b6d83f709f1c0cbf6e38abb786ea674c0e
Esraa <esramuhammed302@gmail.com>
2026-03-29 22:29:49 +0200

    Book Controller

--- models/Book.js
+++ models/Book.js
@@ -5,33 +5,33 @@ const bookSchema = new mongoose.Schema(
     title: {
       type: String,
       required: true,
-      trim: true
+      trim: true,
     },
 
     author: {
       type: String,
-      required: true
+      required: true,
     },
 
     category: {
       type: String,
-      required: true
+      required: true,
     },
 
     price: {
       type: Number,
-      required: true
+      required: true,
     },
 
     availableCopies: {
       type: Number,
       default: 0,
-      min: 0
-    }
+      min: 0,
+    },
   },
   {
-    timestamps: true
-  }
+    timestamps: true,
+  },
 );
 
 module.exports = mongoose.model("Book", bookSchema);
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -2,18 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const bookController = require("../controllers/bookController");
-
 const { protect, admin } = require("../middleware/authMiddleware");
-
 
 router.post("/", protect, admin, bookController.createBook);
 router.put("/:id", protect, admin, bookController.updateBook);
 router.delete("/:id", protect, admin, bookController.deleteBook);
 
-
-
 router.get("/", protect, bookController.getAllBooks);
 router.get("/:id", protect, bookController.getBookById);
-
 
 module.exports = router;
