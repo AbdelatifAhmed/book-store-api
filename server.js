5318378d7ed53bb36cd8945b1ababa037d487fb5
Esraa <esramuhammed302@gmail.com>
2026-03-29 22:50:06 +0200

    Book Controller

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
-require("dotenv").config()
+require("dotenv").config();
 const connectDB = require("./config/db");
+const bookRoutes = require("./routes/bookRoutes");
 const PORT = process.env.PORT || 5000;
 
-
 app.use(express.json());
+app.use("/api/books", bookRoutes);
 
-app.get('/', (req, res) => res.send('Book Store API running'));
+app.get("/", (req, res) => res.send("Book Store API running"));
 
-connectDB()
+connectDB();
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
