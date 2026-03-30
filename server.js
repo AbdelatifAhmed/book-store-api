85fbe384e497c41a08e95d6bef27ab3e154039a3
AbdelatifAhmed <abdoahmed6130@gmail.com>
2026-03-30 09:04:44 +0200

    Delete Duplicate

--- server.js
+++ server.js
@@ -7,20 +7,16 @@ const PORT = process.env.PORT || 5000;
 
 const userRoutes = require("./routes/userRoutes");
 
-const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use("/api/books", bookRoutes);
 
 app.get("/", (req, res) => res.send("Book Store API running"));
+app.use("/api/users", userRoutes);
+app.use("/api/books", bookRoutes);
 
 connectDB();
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => res.send("Book Store API running"));
 
-connectDB().then(() => {
-  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-});
+
