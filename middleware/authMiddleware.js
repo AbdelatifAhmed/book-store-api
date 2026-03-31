0c5d51d4e3d3ea994ac1061319fc6f2e5afcd863
abdellatif ahmed <abdoahmed6130@gmail.com>
2026-03-31 15:49:06 +0200

    fix token problem

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,15 +1,14 @@
 const jwt = require("jsonwebtoken");
 
 function protect(req, res, next) {
-  const token = req.headers.token;
-
+  const token = req.headers.authorization && req.headers.authorization.split(" ")[1];
   if (!token) {
     return res.status(401).json({
       message: "Token required",
     });
   }
 
-  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, payload) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
     if (err) {
       return res.status(403).json({
         message: "Invalid token",
