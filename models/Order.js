a822de69c81c607fa2530bfa3c59c9a841d8c240
Fatma Mohamed <ffatmammohamed110@gmail.com>
2026-03-31 09:59:44 +0200

    Add order book

---
+++ models/Order.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+    {
+
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+
+
+        book: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Book",
+            required: true,
+        },
+
+
+        isReturned: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
