9a7127b5616569500640d1a6eefab26eb8035490
abdellatif ahmed <abdoahmed6130@gmail.com>
2026-03-31 16:42:02 +0200

    delete get user function

--- controllers/userController.js
+++ controllers/userController.js
@@ -58,24 +58,9 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
-const getUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id);
-
-  if (user) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.role,
-    });
-  } else {
-    res.status(404);
-    throw new Error("User not found");
-  }
-});
+;
 
 module.exports = {
   registerUser,
   loginUser,
-  getUserProfile,
 };
