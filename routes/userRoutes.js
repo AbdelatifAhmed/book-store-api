212f3dbf95f755f634f8126b6323c0670021911c
abdellatif ahmed <abdoahmed6130@gmail.com>
2026-04-02 08:35:20 +0200

    Delete comments

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,14 +5,10 @@ const {
   loginUser,
 } = require("../controllers/userController");
 
-// rania put the middleware here like that 
-// const { protect } = require("../middleware/authMiddleware");
 
-// Public routes
+
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 
-// Private route handled by rania 
-// router.get("/profile", getUserProfile);
 
 module.exports = router;
