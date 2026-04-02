799f4f04fe2e469c5abf5b6a13df4c4870ea802f
abdellatif ahmed <abdoahmed6130@gmail.com>
2026-04-02 09:39:45 +0200

    update not found page

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,6 +1,9 @@
 function notFound(req, res, next) {
   const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404);
+  res.status(404).json({
+    status: 404,
+    message: error.message
+  });
   next(error);
 }
 
